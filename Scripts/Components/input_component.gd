b033cc3b3d82be2b534e13e4a5c15018821af574
Eimantasas <152972605+Eimantasas@users.noreply.github.com>
2025-01-16 12:11:52 +0100

    item signal (not working rn)

--- Scripts/Components/input_component.gd
+++ Scripts/Components/input_component.gd
@@ -5,6 +5,10 @@ signal move_input(direction: Vector2)
 @onready var inventory: Node2D = $"../InventoryComponent"
 
 
+func _ready():
+	var item_template_instance = load("res://Scenes/Items/potion.tscn").instantiate()
+	item_template_instance.connect("item_area_entered", _on_item_area_entered)
+
 func _process(_delta: float) -> void:
 	var direction = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
@@ -19,6 +23,8 @@ func _process(_delta: float) -> void:
 	move_input.emit(direction)
 	
 	inventory.print_inventory()
-	
+
+
+func _on_item_area_entered():
 	if Input.is_action_pressed("ui_select"):
 		inventory.add_item({ "name": "Health Potion", "amount": 2 })
