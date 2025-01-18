37c57da2aac10d179c8164f1296fa59aa8bbd622
Arman Ameri <arman.ameri48+Github@gmail.com>
2025-01-18 19:55:42 +0100

    Inventory Basics And Pickup Function DONE

--- Scripts/Components/input_component.gd
+++ Scripts/Components/input_component.gd
@@ -5,9 +5,6 @@ signal move_input(direction: Vector2)
 @onready var inventory: Node2D = $"../InventoryComponent"
 
 
-func _ready():
-	var item_template_instance = load("res://Scenes/Items/potion.tscn").instantiate()
-	item_template_instance.connect("item_area_entered", _on_item_area_entered)
 
 func _process(_delta: float) -> void:
 	var direction = Vector2.ZERO
@@ -22,9 +19,3 @@ func _process(_delta: float) -> void:
 
 	move_input.emit(direction)
 	
-	inventory.print_inventory()
-
-
-func _on_item_area_entered():
-	if Input.is_action_pressed("ui_select"):
-		inventory.add_item({ "name": "Health Potion", "amount": 2 })
