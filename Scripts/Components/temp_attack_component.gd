edaabfbb206aad172acf73985f217ad79cb8cfd6
Arman Ameri <arman.ameri48+Github@gmail.com>
2025-01-22 10:40:59 +0100

    added temp attack

---
+++ Scripts/Components/temp_attack_component.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+@export var attack: CollisionShape2D
+
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("e"):
+		attack.disabled = false
+	else:
+		attack.disabled = true
