2746ebe6be7b160f700109b3552779acc20e62c7
Eimantasas <152972605+Eimantasas@users.noreply.github.com>
2025-01-22 10:40:48 +0100

    adding placeholder walls and input map

--- Scripts/Components/input_component.gd
+++ Scripts/Components/input_component.gd
@@ -9,13 +9,13 @@ signal move_input(direction: Vector2)
 
 func _process(_delta: float) -> void:
 	var direction = Vector2.ZERO
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("d"):
 		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("a"):
 		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("s"):
 		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("w"):
 		direction.y -= 1
 
 	move_input.emit(direction)
