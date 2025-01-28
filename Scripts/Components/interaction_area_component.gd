ff4735d206ffd359d9c9feeef66c12cd20a77cb7
Arman Ameri <arman.ameri48+Github@gmail.com>
2025-01-28 09:23:05 +0100

    added chest function

--- Scripts/Components/interaction_area_component.gd
+++ Scripts/Components/interaction_area_component.gd
@@ -3,12 +3,18 @@ extends Area2D
 class_name InteractionAreaComponent
 
 @export var action_name: String = "interact"
-@export var parent: CharacterBody2D
+@export var parent: Node2D
+@export var item: bool
 
+@onready var description : String
+@onready var image_path : String
 
-@onready var item_name : String = parent.item_name
-@onready var description : String = itemLibrary.get_item_info(item_name, "description")
-@onready var image_path : String = itemLibrary.get_item_info(item_name, "image_path")
+@onready var item_name : String = "healing_potion"
+
+func _ready() -> void:
+	if item:
+		description = itemLibrary.get_item_info(item_name, "description")
+		image_path = itemLibrary.get_item_info(item_name, "image_path")
 
 
 var interact: Callable = func():
