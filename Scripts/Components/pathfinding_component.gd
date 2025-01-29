ee3d1e8f6eebd1f60345829f92e4dcec4e9e47b4
Eimantasas <152972605+Eimantasas@users.noreply.github.com>
2025-01-29 12:57:18 +0100

    pathfinding stuff

--- Scripts/Components/pathfinding_component.gd
+++ Scripts/Components/pathfinding_component.gd
@@ -12,15 +12,17 @@ func _ready() -> void:
 func enemy_setup():
 	await get_tree().physics_frame
 	if player:
-		navigation_agent.target_position = player.global_position
 		ray_cast.look_at(player.global_position)
 
 func _physics_process(_delta: float) -> void:
+	var current_agent_position: Vector2
+	var next_path_position: Vector2
 	enemy_setup()
 	if ray_cast.is_colliding() and (ray_cast.get_collider() is HitboxComponent or ray_cast.get_collider() is Player):
+		if player:
+			navigation_agent.target_position = player.global_position
 		if navigation_agent.is_navigation_finished():
 			return
-		var current_agent_position = global_position
-		var next_path_position = navigation_agent.get_next_path_position()
+		current_agent_position = global_position
+		next_path_position = navigation_agent.get_next_path_position()
 		move_input.emit(current_agent_position.direction_to(next_path_position))
-	else: move_input.emit(Vector2(0, 0))
