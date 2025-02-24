0a0b504d08ee6f95cfe0d0f232e8b8d6b4eb5fba
Arman Ameri <arman.ameri48+Github@gmail.com>
2025-02-24 13:57:47 +0100

    icons

--- Scripts/Components/meele_attack_ai_component.gd
+++ Scripts/Components/meele_attack_ai_component.gd
@@ -4,8 +4,8 @@ signal meele_range_entered()
 signal meele_range_exited()
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	meele_range_entered.emit()
 
-func _on_body_exited(body: Node2D) -> void:
+func _on_body_exited(_body: Node2D) -> void:
 	meele_range_exited.emit()
