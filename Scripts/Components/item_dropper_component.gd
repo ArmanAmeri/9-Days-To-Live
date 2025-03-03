a087757f7f19787811e0ad2f48f043cdcc26ba08
Arman Ameri <arman.ameri48+Github@gmail.com>
2025-03-03 14:24:49 +0100

    added xp and other stuff

--- Scripts/Components/item_dropper_component.gd
+++ Scripts/Components/item_dropper_component.gd
@@ -19,6 +19,6 @@ func drop_items() -> void:
 			var drop = drop_item_path.instantiate()
 			get_node("/root/SceneManager/ItemsOnGround").add_child(drop)
 			drop.global_position = get_parent().global_position
-			drop.velocity = Vector2(randf_range(-100,100), randf_range(-100,100)).rotated(randf_range(-1.5, 1.5)) * randf_range(1.8, 2)
+			drop.velocity = Vector2(randf_range(-120,120), randf_range(-120,120)).rotated(randf_range(-1.5, 1.5)) * randf_range(1.8, 2)
 			
 	pass
